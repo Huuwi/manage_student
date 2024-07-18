7f0ae1126bc79ddced6befcd88a681fcb84b6dcf
Huuwi <110718832+Huuwi@users.noreply.github.com>
2024-07-18 17:35:50 +0700

    first commit

---
+++ backend/model/connection.js
@@ -0,0 +1,7 @@
+const mysql2 = require("mysql2")
+
+
+mysql2 = mysql2.createConnection({
+    host: "localhost",
+    user: "root"
+})
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const connection = require('./model/connection.js')
+
+const app = express()
+
+
+
+
+
+app.listen(6969, () => {
+    console.log("server is running on the port 8080");
+})
